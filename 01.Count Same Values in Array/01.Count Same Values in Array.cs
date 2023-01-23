2cc2fb0c4266e9a7e4acc57b0ba08cce29368220
Martin Todorov <106689924+Martin3255@users.noreply.github.com>
2023-01-23 12:10:08 +0200

    Add files via upload

---
+++ 01.Count Same Values in Array/01.Count Same Values in Array.cs
@@ -0,0 +1,15 @@
+﻿
+Dictionary<double, int> keyValuePairs = new Dictionary<double, int>();
+double[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+for (int i = 0; i < input.Length; i++)
+{
+    if (!keyValuePairs.ContainsKey(input[i]))
+    {
+        keyValuePairs.Add(input[i], 0);
+    }
+    keyValuePairs[input[i]]++;
+}
+foreach (var (value, times) in keyValuePairs)
+{
+    Console.WriteLine($"{value} - {times} times");
+}
